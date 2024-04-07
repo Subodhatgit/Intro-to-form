b45b8c5b8c48e1247dd85646ccfdb4268e6ae8c7
Subodhatgit <subodh.dhungel2023@gmail.com>
2024-04-07 08:39:41 +0545

    Wired up the script with bare functionality

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const submit=document.querySelector(".account");
+const submitButton=document.querySelector(".account");
+const form = document.getElementById("myForm");
+const password1=document.getElementById("password1");
+const password2=document.getElementById("password2");
+const error=document.querySelector('.errorLog');
+submitButton.addEventListener("click",(event)=>{
+    event.preventDefault();
+    if(validateForm()) {
+        form.submit();
+    }else{
+        console.log("Form validation failed!");
+    }
+    
+});
 
-
-submit.addEventListener(("click"),()=>{
-    submit.submit();
-})
+function validateForm(){
+    if(password1.value===password2.value){
+        return true;
+    }else{
+        error.textContent="Passwords do not match!"
+    return false;
+}};
